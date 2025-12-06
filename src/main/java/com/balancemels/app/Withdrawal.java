7c8bc8a0e34751dcef33c46a6203ea429f995aaf
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2025-12-06 18:59:10 +0800

    LB-9 Variations of Transaction Class

---
+++ src/main/java/com/balancemels/app/Withdrawal.java
@@ -0,0 +1,40 @@
+package com.balancemels.app;
+
+public class Withdrawal extends Transaction {
+    public Withdrawal(Builder builder) {
+        this.username = builder.username;
+        this.source = builder.source;
+        this.amount = builder.amount;
+    }
+
+    public static class Builder {
+        private String username;
+        private String source;
+        private double amount;
+
+        public Builder() {}
+
+        public Builder setAmount(double amount) {
+            if (amount >= 0) {
+                throw new IllegalArgumentException(
+                    "Transaction error: expected negative withdrawal amount"
+                );
+            }
+            return this;
+        }
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setSource(String source) {
+            this.source = source;
+            return this;
+        }
+        
+        public Withdrawal build() {
+            return new Withdrawal(this);
+        }
+    }
+}
