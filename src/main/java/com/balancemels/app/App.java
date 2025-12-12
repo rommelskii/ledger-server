3fc8465a4a92ffc3f4b898f2b09a893d025963c7
Rommel John Ronduen <rommel.ronduen2244@gmail.com>
2025-12-13 00:13:20 +0800

    LB-24

--- src/main/java/com/balancemels/app/App.java
+++ src/main/java/com/balancemels/app/App.java
@@ -1,13 +1,24 @@
 package com.balancemels.app;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.balancemels.app.handlers.Request;
 
 public class App 
 {
     public static void main( String[] args ) {
+        /*
+         * try {
+         * User u = Request.getUser("http://localhost:3000/pos_users", "manager_beta");
+         * } catch (Exception e) {
+         * System.err.println(e);
+         * }
+         */
+        User u = new User.Builder().setUsername("rommel")
+                        .setPassword("Password")
+                        .setRunningBalance(212.331)
+                        .build();
+
         try {
-            User u = Request.getUser("http://localhost:3000/pos_users", "manager_beta");
+            Request.createUser("http://localhost:3000/pos_users", u);
         } catch (Exception e) {
             System.err.println(e);
         }
